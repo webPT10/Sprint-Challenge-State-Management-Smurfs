1f768975c0d5c245fa1f8e58cdd2671e8213308a
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-01-10 17:10:10 -0800

    structurred and set up actions/index.js with axios call, exported type(s)

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const FETCH_SMURF_START;
+export const FETCH_SMURF_SUCCESS;
+export const FETCH_SMURF_FAIL;
+
+export const getSmurfs = () => dispatch => {
+    dispatch({ type: FETCH_SMURF_START });
+    axios
+        .get('http://localhost:3333/smurfs')
+        .then(response => {
+            dispatch({ type: FETCH_SMURF_SUCCESS, payload: response.data })
+        })
+        .catch(error => dispatch({ type: FETCH_SMURF_FAIL, payload: error }))
+}
